fbd81a3dc14a8bbdedca0fd84df8fee823adb9f3
jqug <jquinn@cit.mak.ac.ug>
2010-11-29 11:19:57 +0300

    changed path spec

--- src/mainRLVQ.m
+++ src/mainRLVQ.m
@@ -1,12 +1,9 @@
 function [lambda w] = mainRLVQ(data)
 
 close all
-addpath RLVQ/
 
 %pack;
 TrainSetD = data(:,1:end-1);
 TrainLabelsD = data(:,end);
 [lambda w] = RLVQ(TrainSetD, TrainLabelsD);
 
-% ind_rel = ceil(lambda);
-% relfeats = labels(find(ind_rel == 1));
